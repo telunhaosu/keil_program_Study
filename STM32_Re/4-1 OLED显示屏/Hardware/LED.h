a6540a8c70a76e73ae97804a5c650d7acf0b4a3e
HouRunDong <hourundong22@163.com>
2025-01-18 19:21:47 +0800

    更新项目文档以反映最新的功能和配置

--- STM32_Re/4-1 OLED显示屏/Hardware/LED.h
+++ STM32_Re/4-1 OLED显示屏/Hardware/LED.h
@@ -4,9 +4,9 @@
 void LED_Init(void);
 void LED1_ON(void);
 void LED1_OFF(void);
+void LED1_Turn(void);
 void LED2_ON(void);
 void LED2_OFF(void);
-void LED1_Turn(void);
 void LED2_Turn(void);
 
 #endif
