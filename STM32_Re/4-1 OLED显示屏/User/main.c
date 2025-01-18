5d29f5385f644fb68ad53da8cee99b814384b71d
HouRunDong <hourundong22@163.com>
2025-01-18 15:21:02 +0800

    创建了OLED显示屏项目

---
+++ STM32_Re/4-1 OLED显示屏/User/main.c
@@ -0,0 +1,16 @@
+#include "stm32f10x.h" // Device header
+#include "Delay.h"
+#include "LED.h"
+#include "Key.h"
+
+uint8_t KeyNum;
+
+int main(void)
+{
+	LED_Init();
+	Key_Init();
+
+	while (1)
+	{
+	}
+}
