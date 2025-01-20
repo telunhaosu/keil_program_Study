069dee60da0d2ac6b56ffd02f29fcbb77288b07d
HouRunDong <hourundong22@163.com>
2025-01-20 20:37:32 +0800

    定时器计数中断

---
+++ STM32_Re/6-1 定时器中断/System/Timer.c
@@ -0,0 +1,40 @@
+#include "stm32f10x.h"                  // Device header
+
+extern uint16_t Num;
+
+void Timer_Init(void)
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    TIM_InternalClockConfig(TIM2);//配置TIM2时钟源
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1; //利用内部时钟控制采样采样频率
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStructure.TIM_Period = 10000 - 1;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 7200 - 1;
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);
+    TIM_ClearFlag(TIM2, TIM_IT_Update);//避免刚初始化完就进中断
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    
+    //NVIC
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_Init(&NVIC_InitStructure);
+
+    TIM_Cmd(TIM2, ENABLE);
+}
+
+void TIM2_IRQHandler(void)
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+    {
+        Num++;
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
